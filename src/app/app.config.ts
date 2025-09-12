c84e1885b59a5979c9c9a639fa361f04e1dc74b6
minashafik1 <minashafik777@gmail.com>
2025-09-12 15:32:53 +0300

    final merge

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()   // ✅ أضفنا الـ HttpClient هنا
+    provideHttpClient()   
   ]
 };
