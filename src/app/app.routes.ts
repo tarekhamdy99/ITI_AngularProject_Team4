ec9d296b1e70fc218d881efa45a225b3908e9e28
tarekhamdy99 <tarekhamdy7469@gmail.com>
2025-09-12 14:03:54 +0300

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Wishlist } from './components/wishlist/wishlist';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // الصفحة الرئيسية
+  { path: 'home', component: HomeComponent },
   { path: 'wishlist', component: Wishlist, title: 'Wishlist' }
 
 ];
